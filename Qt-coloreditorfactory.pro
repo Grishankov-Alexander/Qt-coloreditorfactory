28b461fbf7d0bea94550992c7b163d01d0d93b95
Grishankov Alexander <grishankov.alexander@gmail.com>
2020-06-19 12:56:01 +0300

    Add sources.

---
+++ Qt-coloreditorfactory.pro
@@ -0,0 +1,11 @@
+QT += widgets
+requires(qtConfig(combobox))
+
+SOURCES += \
+    colorlisteditor.cpp \
+    main.cpp \
+    window.cpp
+
+HEADERS += \
+    colorlisteditor.h \
+    window.h
---
+++ window.h
@@ -0,0 +1,16 @@
+#ifndef WINDOW_H
+#define WINDOW_H
+
+#include <QWidget>
+
+class Window : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Window();
+private:
+    void createGUI();
+};
+
+#endif // WINDOW_H
